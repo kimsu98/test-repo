76be5157a7ece744c1aa6d82e42d1b3c00ff2564
Sung Kim <kimsu98@gmail.com>
2022-11-18 17:25:12 -0800

    Check in test code

---
+++ helloworld/src/main/java/com/alluxio/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+//
+// to execute:
+// java -cp target/helloworld-1.1.jar com.alluxio.helloworld.HelloWorld
+//
+package com.alluxio.helloworld;
+
+public class HelloWorld {
+
+  public static void main(final String[] args) {
+    System.out.println("Hello, World!");
+  }
+
+}
